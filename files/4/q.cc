72cacdf51b96b2f1f9978fa564ae1969e3e9afdc
Tejas <tejas8acharya@gmail.com>
2023-06-03 12:38:47 +0530

    Add CapitalizeFirstLetter definition

--- files/4/q.cc
+++ files/4/q.cc
@@ -1,3 +1,25 @@
 #include <string>
 #include <cctype>
 #include "q.h"
+
+// A function that capitalize the first letter of a string.
+// If it was possible, returns true, otherwise false.
+bool CPPLib::CapitalizeFirstLetter(std::string &input) {
+    if (input.size() == 0) {
+        return false;
+    }
+
+    for (int i = 0, n = input.size(); i < n; i++) {
+        if (isalpha(input[i]) != 0) {
+            if (islower(input[i]) != 0) {
+                input[i] = toupper(input[i]);
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+    return false;
+}
