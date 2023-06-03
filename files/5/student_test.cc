6f2293e868ae9171114ad43b391e9903b14d2b7e
Tejas <tejas8acharya@gmail.com>
2023-06-03 12:55:28 +0530

    Add EmptyFirst test for ConcatenateArray

--- files/5/student_test.cc
+++ files/5/student_test.cc
@@ -24,3 +24,24 @@ TEST(ConcatenateArray, EmptyInput) {
     delete [] array_2;
     delete [] array;
 }
+
+TEST(ConcatenateArray, EmptyFirst) {
+    CPPLib s;
+
+    int size_1 = 0;
+    int size_2 = 3;
+    int size = size_1 + size_2;
+
+    int array_1[size_1];
+    int array_2[size_2] = {1, 2, 3};
+    
+    int *actual = s.Concatenate(array_1, size_1, array_2, size_2);
+    
+    int expected[size] = {1, 2, 3};
+
+    for (int i = 0; i < size; i++) {
+        EXPECT_EQ(expected[i], actual[i]);
+    }
+
+    delete [] actual;
+}
