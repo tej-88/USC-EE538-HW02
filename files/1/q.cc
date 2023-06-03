55353a183a7034f015f19e728c7835dda092eb96
Tejas <tejas8acharya@gmail.com>
2023-06-03 11:54:25 +0530

    Add GetFullNmae definition

--- files/1/q.cc
+++ files/1/q.cc
@@ -2,3 +2,8 @@
 
 #include <iostream>
 #include <vector>
+
+// Given two separate strings for first and last names, return the full name.
+std::string CPPLib::GetFullName(std::string& first_name, std::string& last_name) {
+    return first_name + " " + last_name;
+}
