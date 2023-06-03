82700c4a70adbe408f939a4a4dfa9f6dd85ff068
Tejas <tejas8acharya@gmail.com>
2023-06-03 12:32:42 +0530

    Add Calculate definition

--- files/3/q.cc
+++ files/3/q.cc
@@ -1,3 +1,48 @@
 #include "q.h"
 #include <limits.h>
 
+// Given two integers, returns the result of the operation based on the given
+// operator.
+float CPPLib::Calculate(int a, int b, Operation operation) {
+    float result;
+
+    switch(operation) {
+        case Operation::kAdd :
+            result = a + b;
+            break;
+
+        case Operation::kSubtract :
+            result = a - b;
+            break;
+
+        case Operation::kMultiplication :
+            result = a * b;
+            break;
+        
+        case Operation::kDivision :
+            result = ((float) a) / b;
+            break;
+
+        case Operation::kBitwise_AND :
+            result = a & b;
+            break;
+
+        case Operation::kBitwise_OR :
+            result = a | b;
+            break;
+
+        case Operation::kBitwise_XOR :
+            result = a ^ b;
+            break;
+
+        case Operation::kShift_left :
+            result = a << b;
+            break;
+
+        case Operation::kShift_right :
+            result = a >> b;
+            break;
+    }
+
+    return result;
+}
