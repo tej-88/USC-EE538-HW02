c2ab4e6c08e069cb2b84a88bb5a849d56c5c9a12
Tejas <tejas8acharya@gmail.com>
2023-06-03 13:52:26 +0530

    Add SwapByPointer definition

--- files/7/q.cc
+++ files/7/q.cc
@@ -1,1 +1,15 @@
 #include "q.h"
+
+// Write two functions for swapping variables:
+
+// Example :
+// Before: x = 10, y = 15
+// We call Swap(x,y)
+// After: x = 15, y = 10
+
+void CPPLib::SwapByPointer(float *input1, float *input2) {
+    float tmp = *input1;
+    *input1 = *input2;
+    *input2 = tmp;
+    return;
+}
