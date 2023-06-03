a39ed32334675e149b2f61b24b15adaec4457941
Tejas <tejas8acharya@gmail.com>
2023-06-03 12:12:49 +0530

    Add NoSpace test for SeparateFirstAndLastNames

--- files/2/student_test.cc
+++ files/2/student_test.cc
@@ -79,3 +79,15 @@ TEST(SeparateFirstAndLastNames, EmptyInput) {
     EXPECT_EQ(expected_first, first);
     EXPECT_EQ(expected_last, last);
 }
+
+TEST(SeparateFirstAndLastNames, NoSpace) {
+    CPPLib s;
+    std::string full_name = "John";
+    std::string first;
+    std::string last;
+    s.SeparateFirstAndLastNames(full_name, first, last);
+    std::string expected_first = "John";
+    std::string expected_last;
+    EXPECT_EQ(expected_first, first);
+    EXPECT_EQ(expected_last, last);
+}
