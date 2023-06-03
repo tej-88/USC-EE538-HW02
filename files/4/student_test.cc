6218ab21d4b1b23171d735e4219ec67792d3301a
Tejas <tejas8acharya@gmail.com>
2023-06-03 12:40:10 +0530

    Add NoLetter test for CapitalizeFirstLetter

--- files/4/student_test.cc
+++ files/4/student_test.cc
@@ -16,3 +16,12 @@ TEST(CapitalizeFirstLetter, EmptyInput) {
     EXPECT_EQ(expected, input);
     EXPECT_FALSE(actual_bool);
 }
+
+TEST(CapitalizeFirstLetter, NoLetter) {
+    CPPLib s;
+    std::string input = "123 ";
+    bool actual_bool = s.CapitalizeFirstLetter(input);
+    std::string expected = "123 ";
+    EXPECT_EQ(expected, input);
+    EXPECT_FALSE(actual_bool);
+}
