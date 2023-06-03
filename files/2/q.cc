1e285bd84fafda9c4a0734d851dd31e92ce90564
Tejas <tejas8acharya@gmail.com>
2023-06-03 12:06:12 +0530

    Add IndexOfFirstSpace definition

--- files/2/q.cc
+++ files/2/q.cc
@@ -2,3 +2,18 @@
 
 #include <iostream>
 #include <string>
+
+// Returns the index of the fist space character in a string.
+int CPPLib::IndexOfFirstSpace(std::string &input) {
+    if (input.size() == 0) {
+        return -1;
+    }
+
+    for (int i = 0, n = input.size(); i < n; i++) {
+        if (input[i] == ' ') {
+            return i;
+        }
+    }
+
+    return -1;
+}
