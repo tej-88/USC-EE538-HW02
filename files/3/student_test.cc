77542124f6c3ab9751ff51ce1a65c7e33cca5947
Tejas <tejas8acharya@gmail.com>
2023-06-03 12:33:27 +0530

    Add Add test for Calculate

--- files/3/student_test.cc
+++ files/3/student_test.cc
@@ -7,3 +7,12 @@
 //-----------------------------------------------------------------------------
 // Write some test cases for each function.
 //-----------------------------------------------------------------------------
+
+TEST(Calculate, Add) {
+    CPPLib s;
+    int a = 10;
+    int b = 20;
+    float actual = s.Calculate(a, b, Operation::kAdd);
+    float expected = 30;
+    EXPECT_EQ(expected, actual);
+}
