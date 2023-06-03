df53dd29edc2a1764e906ee94eed4b8dee47d217
Tejas <tejas8acharya@gmail.com>
2023-06-03 12:39:37 +0530

    Add EmptyInput test for CapitalizeFirstLetter

--- files/4/student_test.cc
+++ files/4/student_test.cc
@@ -7,3 +7,12 @@
 //-----------------------------------------------------------------------------
 // Write several test cases for each function.
 //-----------------------------------------------------------------------------
+
+TEST(CapitalizeFirstLetter, EmptyInput) {
+    CPPLib s;
+    std::string input;
+    bool actual_bool = s.CapitalizeFirstLetter(input);
+    std::string expected;
+    EXPECT_EQ(expected, input);
+    EXPECT_FALSE(actual_bool);
+}
