1633b8b4d55fbf5302171772f1dc9bc456625b25
Tejas <tejas8acharya@gmail.com>
2023-06-03 11:58:51 +0530

    Add CountCharacters definition

--- files/1/q.cc
+++ files/1/q.cc
@@ -7,3 +7,29 @@
 std::string CPPLib::GetFullName(std::string& first_name, std::string& last_name) {
     return first_name + " " + last_name;
 }
+
+// Given a string and a list of characters, find how many times any of those
+// characters appeared in the input string. Make the comparison case
+// insensitive. Example: Input: "This is a test", characters = {'t','h'}
+// Output: 4
+
+// YOU CANNOT USE ANY LIBRARY FUNCTIONS FROM std::
+int CPPLib::CountCharacters(std::string& input, std::vector<char> characters) {
+    if (input.size() == 0) {
+        return -1;
+    }
+
+    int count = 0;
+    
+    for (const auto & ch : characters) {
+        for (const auto &lt : input) {
+            if (isalpha(lt)) {
+                if (tolower(ch) == tolower(lt)) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
+}
